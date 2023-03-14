eae9fb93b49b276e51968cd3cdbcc98689160e00
Jamshid Ganiev <jamesganiev227@gmail.com>
2023-03-14 12:16:39 +0900

    Built text get|post using JS & Py

---
+++ get-and-post-api-from-JS/main.py
@@ -0,0 +1,37 @@
+import tornado.web
+import tornado.ioloop
+import json
+
+
+class mainRequestHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("index.html")
+
+
+class listFruitsRequestHandler(tornado.web.RequestHandler):
+    def get(self):
+        fh = open("fruits.txt", 'r')
+        fruits = fh.read().splitlines()
+        fh.close()
+
+        self.write(json.dumps(fruits))
+
+    def post(self):
+        fruit = self.get_argument("fruit")
+        fh = open("fruits.txt", 'a')
+        fh.write(f"\n{fruit}")
+        fh.close()
+
+        self.write(json.dumps({"message": "Fruit added successfully."}))
+
+
+if __name__ == "__main__":
+    app = tornado.web.Application([
+        (r"/", mainRequestHandler),
+        (r"/fruits", listFruitsRequestHandler),
+    ])
+
+port = 8882
+app.listen(port)
+print(f"Application is ready and listening on port:{port}")
+tornado.ioloop.IOLoop.current().start()
